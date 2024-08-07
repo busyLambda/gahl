c8d8964b9b01ffac5bcb79b9a826f352cbca800c
Susa Milán Mihály <mailtosnek@protonmail.com>
2024-08-07 15:13:40 +0200

    Basic lexer.

---
+++ src/lexer/token.rs
@@ -0,0 +1,179 @@
+use std::ops::Range;
+
+#[derive(Debug, Clone)]
+pub struct Span {
+    start: usize,
+    end: usize,
+    literal: String,
+}
+
+impl Span {
+    pub fn new(start: usize, end: usize, literal: String) -> Self {
+        Self {
+            start,
+            end,
+            literal,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum TokenKind {
+    // Types
+    Tvoid,
+    Tbool,
+    Ti8,
+    Ti16,
+    Ti32,
+    Ti64,
+    Ti128,
+    Tu8,
+    Tu16,
+    Tu32,
+    Tu64,
+    Tu128,
+    Tf32,
+    Tf64,
+    Tchar,
+    Tstring,
+    // Keywords
+    KwIf,
+    KwMatch,
+    KwFn,
+    KwImport,
+    KwStruct,
+    KwEnum,
+    // Funnies
+    Dot,
+    At,
+    Comma,
+    RightArrow,
+    Column,
+    Coleq,
+    Eq,
+    EqEq,
+    OpenParen,
+    ClosedParen,
+    OpenCurly,
+    ClosedCurly,
+    OpenBracket,
+    ClosedBracket,
+    // Literals
+    Integer,
+    Identifier,
+    // Ops
+    Add,
+    Min,
+    Mul,
+    Div,
+    Caret,
+    Mod,
+    Not,
+    // Special
+    Whitespace,
+    NewLine, // For sync
+    Unknown,
+    EOF,
+}
+
+impl TokenKind {
+    // Stmt begin, ) } ], \n and such
+    pub fn is_sync_token(&self) -> bool {
+        matches!(
+            self,
+            Self::ClosedParen | Self::ClosedCurly | Self::ClosedBracket | Self::EOF
+        )
+    }
+    pub fn is_begin_new_stmt(&self) -> bool {
+        matches!(self, |Self::KwIf| Self::KwMatch | Self::KwFn)
+    }
+
+    pub fn is_stmt(&self) -> bool {
+        matches!(
+            self,
+            Self::KwIf
+                | Self::KwEnum
+                | Self::KwFn
+                | Self::KwMatch
+                | Self::KwImport
+                | Self::KwStruct
+                | Self::Identifier
+        )
+    }
+
+    pub fn is_type(&self) -> bool {
+        matches!(
+            self,
+            Self::Tvoid
+                | Self::Tbool
+                | Self::Ti8
+                | Self::Ti16
+                | Self::Ti32
+                | Self::Ti64
+                | Self::Ti128
+                | Self::Tu8
+                | Self::Tu16
+                | Self::Tu32
+                | Self::Tu64
+                | Self::Tu128
+                | Self::Tf32
+                | Self::Tf64
+                | Self::Tstring
+                | Self::Tchar
+                | Self::Identifier
+        )
+    }
+
+    pub fn is_expr(&self) -> bool {
+        matches!(self, Self::Integer | Self::Identifier | Self::Min)
+    }
+
+    pub fn is_operator(&self) -> bool {
+        matches!(
+            self,
+            Self::Add
+                | Self::Min
+                | Self::Mul
+                | Self::Div
+                | Self::Mod
+                | Self::Not
+                | Self::Eq
+                | Self::EqEq
+        )
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Token {
+    kind: TokenKind,
+    span: Span,
+    row: usize,
+    col: usize,
+}
+
+impl Token {
+    pub fn new(kind: TokenKind, span: Span, row: usize, col: usize) -> Self {
+        Self {
+            kind,
+            span,
+            row,
+            col,
+        }
+    }
+
+    pub fn row_col(&self) -> (usize, usize) {
+        (self.row, self.col)
+    }
+
+    pub fn kind(&self) -> TokenKind {
+        self.kind
+    }
+
+    pub fn literal(&self) -> String {
+        self.span.literal.clone()
+    }
+
+    pub fn pos(&self) -> Range<usize> {
+        self.span.start..self.span.end
+    }
+}
---
+++ src/lexer/tokenizer.rs
@@ -0,0 +1,160 @@
+use super::{
+    token::{Span, Token, TokenKind},
+    Lexer,
+};
+
+impl Lexer {
+    pub fn lex(&mut self) -> Vec<Token> {
+        let mut tokens = Vec::new();
+
+        while !self.is_eof() {
+            let token = self.next();
+            tokens.push(token);
+        }
+
+        tokens
+            .into_iter()
+            .filter(|t| t.kind() != TokenKind::Whitespace)
+            .collect::<Vec<Token>>()
+    }
+
+    fn next(&mut self) -> Token {
+        if self.is_eof() {
+            return Token::new(
+                TokenKind::EOF,
+                Span::new(0, 0, String::new()),
+                self.row,
+                self.col,
+            );
+        }
+
+        let c = self.eat();
+
+        let kind = match c {
+            c if c.is_alphabetic() => self.ident_or_kw_or_type(),
+            '.' => TokenKind::Dot,
+            ',' => TokenKind::Comma,
+            '(' => TokenKind::OpenParen,
+            ')' => TokenKind::ClosedParen,
+            '{' => TokenKind::OpenCurly,
+            '}' => TokenKind::ClosedCurly,
+            ':' => self.col_or_coleq(),
+            '=' => self.eq_or_eqeq(),
+            '+' => TokenKind::Add,
+            '-' => self.min_or_right_arrow(),
+            '*' => TokenKind::Mul,
+            '/' => TokenKind::Div,
+            '%' => TokenKind::Mod,
+            '^' => TokenKind::Caret,
+            '[' => TokenKind::OpenBracket,
+            ']' => TokenKind::ClosedBracket,
+            '@' => TokenKind::At,
+            '\n' => {
+                self.inc_row();
+                // TokenKind::NewLine
+                TokenKind::Whitespace
+            }
+            c if c.is_numeric() => self.integer(),
+            c if c.is_whitespace() => self.whitespace(),
+            _ => TokenKind::Unknown,
+        };
+
+        let span = self.span();
+
+        let token = Token::new(kind, span, self.row, self.col);
+        self.reset_pos_within_tok();
+
+        token
+    }
+
+    fn ident_or_kw_or_type(&mut self) -> TokenKind {
+        self.eat_while(is_ident_cont);
+
+        match self.tok_str.as_str() {
+            // Keywords
+            "fn" => TokenKind::KwFn,
+            "if" => TokenKind::KwIf,
+            "match" => TokenKind::KwMatch,
+            "import" => TokenKind::KwImport,
+            "struct" => TokenKind::KwStruct,
+            "enum" => TokenKind::KwEnum,
+            // Types
+            "void" => TokenKind::Tvoid,
+            // boolean
+            "bool" => TokenKind::Tbool,
+            // int
+            "i8" => TokenKind::Ti8,
+            "i16" => TokenKind::Ti16,
+            "i32" => TokenKind::Ti32,
+            "i64" => TokenKind::Ti64,
+            "i128" => TokenKind::Ti128,
+            // uint
+            "u8" => TokenKind::Tu8,
+            "u16" => TokenKind::Tu16,
+            "u32" => TokenKind::Tu32,
+            "u64" => TokenKind::Tu64,
+            "u128" => TokenKind::Tu128,
+            // float
+            "f32" => TokenKind::Tf32,
+            "f64" => TokenKind::Tf64,
+            // char, string
+            "char" => TokenKind::Tchar,
+            "string" => TokenKind::Tstring,
+            // Else
+            "not" => TokenKind::Not,
+            _ => TokenKind::Identifier,
+        }
+    }
+
+    fn col_or_coleq(&mut self) -> TokenKind {
+        if let Some(c) = self.peek() {
+            if c == '=' {
+                self.eat();
+                return TokenKind::Coleq;
+            }
+        }
+        TokenKind::Column
+    }
+
+    fn eq_or_eqeq(&mut self) -> TokenKind {
+        if let Some(c) = self.peek() {
+            if c == '=' {
+                self.eat();
+                return TokenKind::EqEq;
+            }
+        }
+        TokenKind::Eq
+    }
+
+    fn whitespace(&mut self) -> TokenKind {
+        self.eat_while(is_whitespace);
+        TokenKind::Whitespace
+    }
+
+    fn integer(&mut self) -> TokenKind {
+        self.eat_while(is_integer_cont);
+        TokenKind::Integer
+    }
+
+    fn min_or_right_arrow(&mut self) -> TokenKind {
+        if let Some(c) = self.peek() {
+            if c == '>' {
+                self.eat();
+                return TokenKind::RightArrow;
+            }
+        }
+        TokenKind::Min
+    }
+}
+
+fn is_ident_cont(c: char) -> bool {
+    c.is_alphanumeric() || c == '_'
+}
+
+fn is_whitespace(c: char) -> bool {
+    c.is_whitespace()
+}
+
+fn is_integer_cont(c: char) -> bool {
+    c.is_numeric()
+}
