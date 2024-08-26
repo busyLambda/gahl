28ccde61ae279c5aa6135b74a36f1d859b9d7161
Susa Milán Mihály <mailtosnek@protonmail.com>
2024-08-26 19:52:39 +0200

    New expression parser for more complex ones that are mutually exclusive to arithmetic

--- src/parser/expr.rs
+++ src/parser/expr.rs
@@ -1,14 +1,16 @@
 use crate::{
-    ast::{Expr, FuncNode, Location},
+    ast::{Expr, FuncNode, Location, Name},
     lexer::{token::TokenKind as TK, Lexer},
-    parser::error::ParseError,
+    parser::{error::ParseError, name::name},
 };
 
-use super::{error::ParseResult, Input};
+use super::{error::ParseResult, stmt::block, Input};
 
 pub fn primary(input: &mut Input) -> ParseResult<Expr> {
     let (first_kind, start_pos, start_row) = match input.peek() {
-        Some(t) => (t.kind(), t.pos(), t.row_col().0),
+        Some(t) => {
+            (t.kind(), t.pos(), t.row_col().0)
+        }
         None => todo!(),
     };
 
@@ -21,6 +23,15 @@ pub fn primary(input: &mut Input) -> ParseResult<Expr> {
                 vec![],
                 false,
             )
+        }
+        TK::Identifier => {
+            let (name, errors, is_eof) = name(input);
+            let location = Location::new(
+                start_pos.start..input.prev_pos.end,
+                (start_row, input.prev_row),
+            );
+
+            (Expr::Identifier(name, location), errors, is_eof)
         }
         TK::OpenParen => {
             input.eat();
@@ -198,11 +209,6 @@ fn factor(input: &mut Input) -> ParseResult<Expr> {
     (expr, errors, is_eof)
 }
 
-// Expr, Expr
-pub fn tuple_expr(input: &mut Input) -> ParseResult<Expr> {
-    todo!()
-}
-
 pub fn function_expr(input: &mut Input) -> ParseResult<FuncNode> {
     input.eat().unwrap();
 
@@ -232,8 +238,8 @@ pub fn function_expr(input: &mut Input) -> ParseResult<FuncNode> {
         };
 
         match input.expect(TK::Identifier) {
-            Ok(Some(_)) => {
-                product.args.push(input.eat().unwrap().literal());
+            Ok(Some(ident)) => {
+                product.args.push(ident.literal());
             }
             Err((error, is_stmt)) => {
                 product.errors.push(error);
@@ -242,12 +248,58 @@ pub fn function_expr(input: &mut Input) -> ParseResult<FuncNode> {
                 } else {
                     input.eat();
                 }
-            },
+            }
             Ok(None) => return (product, vec![], true),
         };
     }
 
+    match input.peek() {
+        Some(t) if t.kind() == TK::OpenCurly => {
+            let (block, errors, is_eof) = block(input);
+            product.block = block;
+
+            // Add errors.
+            if is_eof {
+                return (product, vec![], is_eof);
+            }
+        }
+        Some(t) => todo!(),
+        None => return (product, vec![], true),
+    };
+
     (product, vec![], false)
+}
+
+pub fn expression(input: &mut Input) -> ParseResult<Expr> {
+    let mut errors: Vec<ParseError> = vec![];
+    let mut product = Expr::Void;
+
+    let first_kind = match input.peek() {
+        Some(t) => t.kind(),
+        None => return (product, errors, true),
+    };
+
+    if first_kind == TK::KwFn {
+        let (fn_expr, mut fn_expr_errors, is_eof) = function_expr(input);
+
+        product = Expr::Func(fn_expr);
+        errors.append(&mut fn_expr_errors);
+
+        if is_eof {
+            return (product, errors, is_eof);
+        }
+    } else {
+        let (expr, mut expr_errors, is_eof) = expr(input);
+
+        product = expr;
+        errors.append(&mut expr_errors);
+
+        if is_eof {
+            return (product, errors, is_eof);
+        }
+    }
+
+    return (product, errors, false);
 }
 
 #[test]
