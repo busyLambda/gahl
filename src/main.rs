5c18c8f67b1b99ac5b8f30108ad197fe084c6fc6
Susa Milán Mihály <mailtosnek@protonmail.com>
2024-09-08 21:08:45 +0200

    Call things in main.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+use std::fs;
+
+use checker::Checker;
+use lexer::Lexer;
+use parser::{module, Input};
+
 pub mod ast;
+pub mod checker;
 pub mod lexer;
 pub mod parser;
 
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("examples/main.gh").expect("Cannot find `examples/main.gh`");
+
+    let mut lexer = Lexer::new(&input);
+    let tokens = lexer.lex();
+    let mut parser = Input::new(tokens);
+
+    let module = module(&mut parser, "examples/main.gh".to_string());
+
+    {
+        let mut checker = Checker::new(&module);
+        checker.types();
+        
+        checker.print_interrupts();
+    }
 }
